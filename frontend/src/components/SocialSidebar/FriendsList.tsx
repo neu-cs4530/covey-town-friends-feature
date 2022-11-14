a84ae0cf016a8456fe47c9315519af6e79239d73
unknown <chlo88@optonline.net>
2022-11-14 15:11:13 -0500

    small typo fix.2

--- frontend/src/components/SocialSidebar/FriendsList.tsx
+++ frontend/src/components/SocialSidebar/FriendsList.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import useTownController from '../../hooks/useTownController';
 
 /**
- * Lists the current friends of this TownController.ouPlayer, along with the buttons to invite
+ * Lists the current friends of this TownController.ourPlayer, along with the buttons to invite
  * selected friends to a conversation area, view conversation area invites, and send a brief
  * message to all selected friends.
  *
@@ -14,7 +14,8 @@ export default function FriendsInTownList(): JSX.Element {
   const { friendlyName, townID } = useTownController();
 
   // IMPORTANT NOTE: Remember that you can define buttons elsewhere, and then just import them
-  // and add them where necessary here. ==> TODO: remove this comment in Sprint 3.
+  // and add them where necessary here.
+  // TODO: remove the above comment in Sprint 3.
 
   return (
     <Box>
